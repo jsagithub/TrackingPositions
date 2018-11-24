939aa6faf152203752143ffaf28f8bebe296d61a
jsagithub <jsa_epe@hotmail.com>
2018-11-24 17:22:54 +0000

    inputs page add-tracking

---
+++ src/pages/addtracking/addtracking.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AddtrackingPage } from './addtracking';
+
+@NgModule({
+  declarations: [
+    AddtrackingPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AddtrackingPage),
+  ],
+})
+export class AddtrackingPageModule {}
